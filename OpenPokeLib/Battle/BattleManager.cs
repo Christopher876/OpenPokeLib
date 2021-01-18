9ae59338b11b0258f260f70037940da355412370
Christopher Williams <christopher876@outlook.com>
2021-01-18 12:02:25 -0500

    Add more basics to the battle system
    - Pokemon can now be seleted as to which one is battling
    - No battling and move damage calculation has been implemented

--- OpenPokeLib/Battle/BattleManager.cs
+++ OpenPokeLib/Battle/BattleManager.cs
@@ -1,18 +1,48 @@
+using System;
+using System.Diagnostics;
+
 namespace OpenPokeLib.Battle
 {
     public class BattleManager
     {
-        private Trainer Player;
+        private readonly bool _isTrainer;
         private Trainer Opponent;
 
-        public BattleManager()
+        private Pokemon CurrentPlayerPokemon;
+        private Pokemon CurrentOpponentPokemon;
+
+        public BattleManager(bool isTrainer, Pokemon WildPokemon = null)
         {
-            
+            _isTrainer = isTrainer;
+            if (WildPokemon != null)
+            {
+                CurrentOpponentPokemon = WildPokemon;
+            }
         }
 
-        public void SwitchPokemon(Pokemon pokemon)
+        public void SwitchPokemon(int slot)
         {
-            
+            CurrentPlayerPokemon = GlobalObjectManager.instance.Player.Team[slot];
+            Debug.WriteLine($"Switched to {CurrentPlayerPokemon}");
+        }
+
+        public void SetCurrentActivePlayerPokemon(int slot)
+        {
+            //TODO Check if that Pokemon exists in the slot
+            CurrentPlayerPokemon = GlobalObjectManager.instance.Player.Team[slot];
+            Debug.WriteLine($"Player Pokemon set to {CurrentPlayerPokemon}");
+        }
+
+        public void SetCurrentActiveOpponentPokemon(int slot)
+        {
+            CurrentOpponentPokemon = Opponent.Team[slot];
+            Debug.WriteLine($"Opponent Pokemon set to {CurrentPlayerPokemon}");
+        }
+
+        public void DefeatPokemon()
+        {
+            var gainedExp = ExperienceCalculation.CalculateExp(GlobalObjectManager.instance.Player, CurrentPlayerPokemon, CurrentOpponentPokemon, _isTrainer);
+            CurrentPlayerPokemon.GainExp(gainedExp);
         }
     }
 }
